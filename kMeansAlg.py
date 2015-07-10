cd19928eea0b67116baf7865c1c67128748cdb8f
kcastro90 <kcastro@student.bridgew.edu>
2015-07-10 00:34:59 -0400

    Update kMeansAlg.py
    So far this prints all the elements that are to be used as stats values.

--- kMeansAlg.py
+++ kMeansAlg.py
@@ -1,12 +1,30 @@
 __author__ = 'karic_000'
 
+import random
 import csv
-import sys
+
+#import sys
 
 csvfile = open('simple-soccer-database.csv', 'rt')
+
+#Create 5 clusters
+cluster1 = []
+cluster2 = []
+cluster3 = []
+cluster4 = []
+cluster5 = []
+elements = []
 try:
     reader = csv.reader(csvfile)
-    for row in reader:
-        print(row)
+    for col in reader:
+        #12 columns and 21 rows
+        #omit first row
+        elements.append(col[1:12])
+    #print (elements)
+    #11*21 = 231 elements
+    stats = elements[11:230]
+    print (stats)
+
+
 finally:
     csvfile.close()
