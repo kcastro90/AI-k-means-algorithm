08658ec3be4027ea2e9267a203f4b2c2b0d59991
kcastro90 <kcastro@student.bridgew.edu>
2015-07-15 13:02:54 -0400

    Update kMeansAlg.py

--- kMeansAlg.py
+++ kMeansAlg.py
@@ -2,31 +2,58 @@ __author__ = 'karic_000'
 import random
 import csv
 
-#import sys
 
-csvfile = open('simple-soccer-database.csv', 'rt')
+csv_file = open('simple-soccer-database.csv', 'rt')
 elements = []
 stats = []
 
 try:
-    reader = csv.reader(csvfile)
-    print ("\n THIS WILL BE THE OUTPUT WITHOUT INCLUDING THE FIRST COLUMN:\n")
+    reader = csv.reader(csv_file)
+    print("\n THIS WILL BE THE OUTPUT WITHOUT INCLUDING THE FIRST COLUMN:\n")
     for col in reader:
-        print (col[1::])
-        #omit first COLUMN
+        print(col[1::])
+        # omit first COLUMN
         elements.append(col[1::])
 
-    print ("\n THIS WILL BE THE NEW ARRAY OF ELEMENTS (including column label):\n")
-    print (elements)
+    print("\n THIS WILL BE THE ARRAY OF ELEMENTS (including column label):\n")
+    print(elements)
 
-    print ("\n THIS WILL BE THE DATA TB BE USED (it excludes the 12 column labels):\n")
+    print("\n THIS WILL BE THE DATA TB BE USED (it excludes the 12 column labels):\n")
     stats = elements[1::]
-    print (stats)
+    print(stats)
 
-    dimensions = len(stats) * 11
-    #20 rows 11 columns in example; dimension 220
+    rowAmount = len(stats)
+    dimensions = rowAmount * 11
+    # 20 rows 11 columns
+    # print(stats[0][1]) 31
+
+    # Create 5 clusters
+    clusters = [[] for i in range(4)]
+
+    # Assign each observation/element to a random cluster
+    # Loop over all observations
+    for observation in stats:
+        clusterIndex = random.choice(clusters)
+        cluster = clusters[clusterIndex]
+        cluster.append(observation)
+        print(clusters)
+
+    # done = False
+    # while not done
+        # sourceIndex = ranInt(K)
+        # source = clusters[sourceIndex]
+        # if (source not cluster && source.length > 1):
+            # sourceObservationIndex = rndInt(source.length)
+            # sourceObservation = source[sourceObservationIndex]
+            # source.remove(sourceObservationIndex)
+            # cluster.add(sourceObservation)
+            # done = True
+    # updateStep()
+
+
 finally:
-    csvfile.close()
+    csv_file.close()
+
 
 
 
