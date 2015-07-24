877109ba700468de6e66f98667f0b2ec963823c0
kcastro90 <kcastro@student.bridgew.edu>
2015-07-24 01:17:37 -0400

    Update kMeansAlg.py

--- kMeansAlg.py
+++ kMeansAlg.py
@@ -1,7 +1,6 @@
 __author__ = 'karic_000'
 import random
 import csv
-#import numpy as np
 
 csv_file = open('simple-soccer-database.csv', 'rt')
 elements = []
@@ -9,44 +8,56 @@ stats = []
 
 try:
     reader = csv.reader(csv_file)
+    next(reader)    # Skip headers.
+    ints = [[int(item) for number, item in enumerate(row) if item and (1 <= number <=12)] for row in reader]
+    print(ints)
+    
     print("\n THIS WILL BE THE OUTPUT WITHOUT INCLUDING THE FIRST COLUMN:\n")
     for col in reader:
         print(col[1::])
         # omit first COLUMN
-        elements.append(col[1::])
+        elements.append(col[1::])   # THIS WILL BE THE ARRAY OF ELEMENTS (including column label)
 
-    print("\n THIS WILL BE THE ARRAY OF ELEMENTS (including column label):\n")
-    print(elements)
-
-    print("\n THIS WILL BE THE DATA TB BE USED (it excludes the 12 column labels):\n")
+    print("\n THIS WILL BE THE DATA TO BE USED (it excludes the 12 column labels):\n")
     stats = elements[1::]
     print(stats)
 
     rowAmount = len(stats)
     colAmount = 11
     dimensions = rowAmount * colAmount  # 20 rows 11 columns in this example
-    cluster = [[] for i in range(4)]    # Create 5 empty clusters
+    sums = stats[0][0] + stats[0][1]
+    #print(sums)
+    cluster = [[] for i in range(4)]    # Creates 5 empty clusters
+    # clusters = [], [], [], [], []
 
     # ClusterListSize defines how many rows from "stats" can each cluster have
     clusterListSize = rowAmount//5
+    print("\nEach cluster should have this many rows:")
+    print(clusterListSize)
 
-    random_choice1 = random.sample(stats, clusterListSize)
-    print(random_choice1)
-    
-    """NOW I'M TRYING TO ASSIGN A random_choice LIST OF ROWS TO EACH OF THE 5 CLUSTERS
-    WITHOUT REPEATING ANY OF THE ROWS
-    BEYOND THIS POINT IT'S ALL IRRELEVANT AT THIS STAGE"""
+    index = 0
+    #for aRow in stats: # Go through each row in stat
+        #for search_repeat in cluster[index]: # Search through all elements in that cluster
 
-    
-    # randOberservation = np.random.randint(dimensions, size=(rowAmount,colAmount))
+    random_row_index = random.randint(0,rowAmount)
+    print("\nrandom_row_index:")
+    print(random_row_index)
+    cluster[index].append(random_row_index)
+    del stats[random_row_index]
+    print("\nnew stats:")
+    print(stats)
+
+
+    #MAKE STRINGS INTO INTEGERS
+
+    """def centroids(lst)
+    mean = sum(lst) / n
+    return mean"""
+
+
     # Assign each observation/element to a random cluster
     # Loop over all observations
-    """for observation in stats:
-        clusterIndex = random.choice(clusters)
-        # cluster = clusters[clusterIndex]
-        # cluster.append(observation)
-        clusters[clusterIndex].append(observation)
-        print(clusters)"""
+
 
     # done = False
     # while not done
